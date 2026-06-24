572302d28610f6b6a7233c4134705a33ae45b27e
Andrei Borza <andrei.borza@sentry.io>
2026-06-24 16:44:38 +0200

    Pass normalizedRequest to the sampling context for root spans

--- packages/core/src/tracing/trace.ts
+++ packages/core/src/tracing/trace.ts
@@ -513,6 +513,7 @@ function _startRootSpan(
           name,
           parentSampled: finalParentSampled,
           attributes: finalAttributes,
+          normalizedRequest: isolationScope.getScopeData().sdkProcessingMetadata.normalizedRequest,
           parentSampleRate: parseSampleRate(currentPropagationContext.dsc?.sample_rate),
         },
         currentPropagationContext.sampleRand,
